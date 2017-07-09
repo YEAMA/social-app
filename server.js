5dade4057ccbe26f691d0f104b86b3c8ec3cf5d8
YEAMA <youssefemad99@hotmail.com>
2017-07-10 00:17:56 +0200

    Twitter API test

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require('express');
 var mongoose = require('mongoose');
 var session = require('express-session');
 const hbs = require('hbs');
-const { ObjectID } = require('mongodb');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+// const Twit = require('twit')
+const Twitter = require("node-twitter-api");
+
+const { ObjectID } = require('mongodb');
 
 var app = express();
 require('dotenv').load();
 
-// const baseURL = "https://fcc-y-voting-app.herokuapp.com";
+const baseURL = "https://fcc-socialapp.herokuapp.com/";
 
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
@@ -39,6 +42,14 @@ app.use(session({
 
 axios.defaults.headers.common['Authorization'] = 'Bearer CKJIm1aKHYxpJLfIxo8TrXwOLyM2hvdiqzvDqqethjoAB-R17AqFO3CrHnmIGvzup864GershFcC2UMQ65ylLkXv1ke4dR_Nh34m87DWba46tTFXfWKxOzwG6kREWXYx';
 
+var twitter = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    callback: process.env.CALLBACK_URL
+})
+
+var _requestSecret;
+
 app.get('/', (req, res) => {
     axios.get('https://api.yelp.com/v3/businesses/search?location=washington')
         .then((response) => {
@@ -50,7 +61,51 @@ app.get('/', (req, res) => {
         .catch((e) => console.log(e))
 })
 
+app.get('/fetch_rt', (req, res) => {
+    // T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+    //     res.send(data)
+    // })
 
+    // Change screen name to be a variable carrying the name of the loggedin user
+    // T.get('followers/ids', { screen_name: 'YEWDeveloper' }, function(err, data, response) {
+    //     res.send(data)
+    // })
+
+    // T.get('account/verify_credentials', { skip_status: true })
+    //     .catch(function(err) {
+    //         console.log('caught error', err.stack)
+    //     })
+    //     .then(function(result) {
+    //         res.send(result); // Sends logged in user data
+    //     })
+
+    twitter.getRequestToken(function(err, requestToken, requestSecret) {
+        if (err)
+            res.status(500).send(err);
+        else {
+            _requestSecret = requestSecret;
+            res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+        }
+    });
+
+});
+
+app.get('/fetched_rt', (req, res) => {
+    var requestToken = req.query.oauth_token,
+        verifier = req.query.oauth_verifier;
+
+    twitter.getAccessToken(requestToken, _requestSecret, verifier, function(err, accessToken, accessSecret) {
+        if (err)
+            res.status(500).send(err);
+        else
+            twitter.verifyCredentials(accessToken, accessSecret, function(err, user) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.send(user);
+            });
+    });
+});
 
 app.get('/logout', function(req, res) {
     if (req.session.user)
